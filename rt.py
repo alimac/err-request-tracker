f54e095702754b11a659c326dd747eedef5233e0
Alina Mackenzie <alina.mackenzie@gmail.com>
2015-11-14 13:00:25 -0600

    Improve configuration checks
    Addresses #3.

--- rt.py
+++ rt.py
@@ -1,6 +1,7 @@
 import re
 import rt
 from itertools import chain
+from errbot.utils import ValidationException
 from errbot import BotPlugin, botcmd, re_botcmd
 
 CONFIG_TEMPLATE = { 'USER':'',
@@ -28,17 +29,25 @@ class RT(BotPlugin):
 
     def check_configuration(self, config):
 
+        rt_login = False
+
         for key in ['REST_URL','DISPLAY_URL','USER','PASSWORD']:
 
            if key not in config:
-               raise Exception(key + "is missing")
+               raise ValidationException("missing config value: " + key)
 
-    def rt_login(self):
-        if self.tracker:
-           return
+        try:
+           tracker = rt.Rt('%s/REST/1.0/' % config['REST_URL'])
+           rt_login = tracker.login(config['USER'], config['PASSWORD'])
 
-        self.tracker = rt.Rt(self.config['REST_URL'])
-        response = self.tracker.login(self.config['USER'], self.config['PASSWORD'])
+        except Exception as error:
+           raise ValidationException("Cannot connect to RT as %s: %s." % (
+               config['USER'], format(error),
+           ))
+
+        if rt_login is False:
+           raise ValidationException("Authentication failed")
+
 
     @re_botcmd(pattern=r'(^| |https?\:\/\/.+=)(\d{1,})( |\?|\.|,|:|\!|$)', prefixed=False, flags=re.IGNORECASE)
     def find_ticket(self, message, match):
@@ -54,7 +63,8 @@ class RT(BotPlugin):
 
     def ticket_summary(self, ticket_id):
 
-        self.rt_login()
+        self.tracker = rt.Rt(self.config['REST_URL'])
+        response = self.tracker.login(self.config['USER'], self.config['PASSWORD'])
 
         try:
             ticket = self.tracker.get_ticket(ticket_id)
