4686793ee1a5299cbb1d6660813f27665937ca28
Alina Mackenzie <alina.mackenzie@gmail.com>
2015-11-14 20:14:32 -0600

    Add test for finding a ticket

--- test_requesttracker.py
+++ test_requesttracker.py
@@ -17,3 +17,28 @@ class TestRT(object):
     def test_configuration(self, testbot):
         testbot.push_message('!plugin config RT ' + str(RT_CONFIG))
         assert 'Plugin configuration done.' in testbot.pop_message()
+
+    def test_find_ticket(self, testbot):
+        test_subject = 'err-request-tracker'
+        test_requestor = 'foo@example.com'
+
+        testbot.push_message('!plugin config RT ' + str(RT_CONFIG))
+        assert 'Plugin configuration done.' in testbot.pop_message()
+
+        tracker = rt.Rt(RT_CONFIG['REST_URL'])
+        tracker.login(RT_CONFIG['USER'], RT_CONFIG['PASSWORD'])
+
+        try:
+            ticket_id = tracker.create_ticket(Queue='General',
+                                              Subject=test_subject,
+                                              Requestors=test_requestor)
+            ticket = tracker.get_ticket(ticket_id)
+        except Exception as e:
+            raise Exception("Unable to create a test ticket: " + str(e))
+
+        testbot.push_message(str(ticket_id))
+        assert "%s (%s%s) in General from %s" % (
+            test_subject,
+            RT_CONFIG['DISPLAY_URL'],
+            str(ticket_id),
+            test_requestor) in testbot.pop_message()
